e041502ec0d307401a16cb7be804395c38b01f52
bmq928 <bmq928@gmail.com>
2024-08-24 00:14:38 +0700

    feat(frontend): UI

--- apps/frontend/app/home/page.tsx
+++ apps/frontend/app/home/page.tsx
@@ -1,3 +1,104 @@
+'use client'
+
+import CancelIcon from '@mui/icons-material/Cancel'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import SaveIcon from '@mui/icons-material/Save'
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridRowModes,
+  GridRowModesModel,
+} from '@mui/x-data-grid'
+import { useState } from 'react'
+
 export default function Page() {
-  return <div>home</div>
+  const [rows] = useState([{ id: 1, name: '1', description: '1' }])
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({})
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
+    {
+      field: 'description',
+      headerName: 'Description',
+      width: 500,
+      editable: true,
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              key={`${id}-Save`}
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: 'primary.main',
+              }}
+              onClick={() =>
+                setRowModesModel((prev) => ({
+                  ...prev,
+                  [id]: { mode: GridRowModes.View },
+                }))
+              }
+            />,
+            <GridActionsCellItem
+              key={`${id}-Cancel`}
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={() =>
+                setRowModesModel((prev) => ({
+                  ...prev,
+                  [id]: { mode: GridRowModes.View, ignoreModifications: true },
+                }))
+              }
+              color="inherit"
+            />,
+          ]
+        }
+
+        return [
+          <GridActionsCellItem
+            key={`${id}-Edit`}
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={() =>
+              setRowModesModel((prev) => ({
+                ...prev,
+                [id]: { mode: GridRowModes.Edit },
+              }))
+            }
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            key={`${id}-Delete`}
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={() => console.log({ id })}
+            color="inherit"
+          />,
+        ]
+      },
+    },
+  ]
+
+  return (
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      rowModesModel={rowModesModel}
+      editMode="row"
+      onRowModesModelChange={setRowModesModel}
+    />
+  )
 }
--- apps/frontend/app/register/page.tsx
+++ apps/frontend/app/register/page.tsx
@@ -1,3 +1,80 @@
+'use client'
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import Link from 'next/link'
+import * as React from 'react'
+
 export default function Page() {
-  return 'register'
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    })
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/login">{'Already have an account? Log in'}</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
